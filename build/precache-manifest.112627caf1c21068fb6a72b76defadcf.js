2377c1681e070fcb2e85b47c628b4b991d0b17c0
Infinityrs <68905131+Infinityrs@users.noreply.github.com>
2022-09-26 16:32:12 +0530

    Add files via upload

---
+++ build/precache-manifest.112627caf1c21068fb6a72b76defadcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d164927adfd6501a40ead9c02b7f1534",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85680369aed9da8ed0a9",
+    "url": "/static/css/main.cfeb5ae3.chunk.css"
+  },
+  {
+    "revision": "8abd37dd48d85c823823",
+    "url": "/static/js/2.0b7e2a6c.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/static/js/2.0b7e2a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85680369aed9da8ed0a9",
+    "url": "/static/js/main.bb0e88ac.chunk.js"
+  },
+  {
+    "revision": "37de3dbaed0fbb943091",
+    "url": "/static/js/runtime-main.6ce75eaa.js"
+  },
+  {
+    "revision": "348d66fdc31f6639354ab9785d05af38",
+    "url": "/static/media/books.348d66fd.svg"
+  }
+]);
